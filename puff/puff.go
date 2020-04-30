ad37135bba187259506f7ad19268ec854816257e
marxn <wywinxp@163.com>
2020-04-30 17:14:09 +0800

    regular

--- puff/puff.go
+++ puff/puff.go
@@ -27,11 +27,11 @@ type directoryInfo struct {
 }
 
 func ExecShellCmd(s string) (string, error) {
-	cmd := exec.Command("/bin/bash", "-c", s)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	return out.String(), err
+    cmd := exec.Command("/bin/bash", "-c", s)
+    var out bytes.Buffer
+    cmd.Stdout = &out
+    err := cmd.Run()
+    return out.String(), err
 }
 
 func emptyDir(path string) bool {
@@ -106,35 +106,35 @@ func getExportFuncList(fileList []string) ([]funcItem, error) {
     var result []funcItem
     for _, filename := range fileList {
         fset := token.NewFileSet()
-    	f, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
-    	if err != nil {
-    		panic(err)
-    	}
-    	for _, value := range f.Decls {
-    		t := reflect.TypeOf(value)
-    		s := t.String()
-    		if strings.Contains(s, "FuncDecl") {
-    		    decl := value.(*ast.FuncDecl)
-    			funcName := decl.Name
-    			if !isExported(fmt.Sprintf("%s", funcName)) {
-    			    continue
-    			}
-    			funcDesc := &funcItem{FuncName: fmt.Sprintf("%s", funcName)}
-    			doc  := decl.Doc
-            	if doc!=nil {
-            	    for _, docItem := range doc.List {
-                	    headByte := []byte(docItem.Text)
-                	    if string(headByte[0:3])=="///" && len(headByte[3:]) > 8 {
-                	        if funcDesc.Comment==nil {
-                	            funcDesc.Comment = make([]string, 0)
-                	        }
-                	        funcDesc.Comment = append(funcDesc.Comment, string(headByte[3:]))
-                	    }
-                	}
-            	}
-            	if funcDesc.Comment!=nil {
-    			    result = append(result, *funcDesc)
-    			}
+        f, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+        if err != nil {
+            panic(err)
+        }
+        for _, value := range f.Decls {
+            t := reflect.TypeOf(value)
+            s := t.String()
+            if strings.Contains(s, "FuncDecl") {
+                decl := value.(*ast.FuncDecl)
+                funcName := decl.Name
+                if !isExported(fmt.Sprintf("%s", funcName)) {
+                    continue
+                }
+                funcDesc := &funcItem{FuncName: fmt.Sprintf("%s", funcName)}
+                doc  := decl.Doc
+                if doc!=nil {
+                    for _, docItem := range doc.List {
+                        headByte := []byte(docItem.Text)
+                        if string(headByte[0:3])=="///" && len(headByte[3:]) > 8 {
+                            if funcDesc.Comment==nil {
+                                funcDesc.Comment = make([]string, 0)
+                            }
+                            funcDesc.Comment = append(funcDesc.Comment, string(headByte[3:]))
+                        }
+                    }
+                }
+                if funcDesc.Comment!=nil {
+                    result = append(result, *funcDesc)
+                }
             }
         }
     }
@@ -159,16 +159,16 @@ func main() {
     var taskHolder     []string
     var groupHolder   []string
     
-	input              := flag.String("i", "", "input source file directory")
-	vascConfigFileName := flag.String("c", "", "vasc config file")
-	output             := flag.String("o", "", "output source file")
+    input              := flag.String("i", "", "input source file directory")
+    vascConfigFileName := flag.String("c", "", "vasc config file")
+    output             := flag.String("o", "", "output source file")
     
-	flag.Parse()
+    flag.Parse()
 
-	if *input == "" || *output == "" {
-		fmt.Println("invalid arguments")
-		return
-	}
+    if *input == "" || *output == "" {
+        fmt.Println("invalid arguments")
+        return
+    }
 
     sourceInfo := make(map[string]*directoryInfo)
     
@@ -176,37 +176,37 @@ func main() {
     if err!=nil {
         panic(err)
     }
-	
-	for _, value := range dirList {
-	    fmt.Println("analyzing directory:" + value)
-	    dirInfo := new(directoryInfo)
-	    dirInfo.Dir = value
-    	fileList, err := getFileList(value)
-    	if err!=nil {
-    	    panic(err)
-    	}
-    	
-	    dirInfo.FileList = fileList
-	    funcList, err := getExportFuncList(fileList)
-	    if err!=nil {
-	        panic(err)
-	    }
-	    if len(funcList) > 0 {
-	        dirInfo.FuncList   = funcList
-	        dirInfo.NeedExport = true
-	    }
-	    sourceInfo[value] = dirInfo
-	}
-	
-	source := fmt.Sprintf("//Vasc generated code. Do not modify.\n\npackage main\n\nimport \"github.com/marxn/vasc\"\nimport \"github.com/marxn/vasc/global\"\n")
-	
-	for _, value := range sourceInfo {
-	    if value.NeedExport {
-	        source += fmt.Sprintf("import %s \"%s\"\n", replacePackagePath(value.Dir), value.Dir)
-	    }
-	}
-	
-	source += "\n\nvar VascFuncMap = map[string]interface{}{\n"
+    
+    for _, value := range dirList {
+        fmt.Println("analyzing directory:" + value)
+        dirInfo := new(directoryInfo)
+        dirInfo.Dir = value
+        fileList, err := getFileList(value)
+        if err!=nil {
+            panic(err)
+        }
+        
+        dirInfo.FileList = fileList
+        funcList, err := getExportFuncList(fileList)
+        if err!=nil {
+            panic(err)
+        }
+        if len(funcList) > 0 {
+            dirInfo.FuncList   = funcList
+            dirInfo.NeedExport = true
+        }
+        sourceInfo[value] = dirInfo
+    }
+    
+    source := fmt.Sprintf("//Vasc generated code. Do not modify.\n\npackage main\n\nimport \"github.com/marxn/vasc\"\nimport \"github.com/marxn/vasc/global\"\n")
+    
+    for _, value := range sourceInfo {
+        if value.NeedExport {
+            source += fmt.Sprintf("import %s \"%s\"\n", replacePackagePath(value.Dir), value.Dir)
+        }
+    }
+    
+    source += "\n\nvar VascFuncMap = map[string]interface{}{\n"
 
     for _, sourceCode := range sourceInfo {
         for _, funcCall := range sourceCode.FuncList {
@@ -229,84 +229,84 @@ func main() {
             }
         }
     }
-	source += fmt.Sprintf("}\n")
-	
-	configFile, err := ioutil.ReadFile(*vascConfigFileName)
+    source += fmt.Sprintf("}\n")
+    
+    configFile, err := ioutil.ReadFile(*vascConfigFileName)
     if err!=nil {
-	    panic(err)
-	}
-	source += fmt.Sprintf("\n\nvar configFile = `%s`", configFile)
-	
-	appConfigFile := fmt.Sprintf("\n{\n")
-	appConfigFile += fmt.Sprintf("        \"schedule_list\": [\n")
-	for index, schedule := range scheduleHolder {
+        panic(err)
+    }
+    source += fmt.Sprintf("\n\nvar configFile = `%s`", configFile)
+    
+    appConfigFile := fmt.Sprintf("\n{\n")
+    appConfigFile += fmt.Sprintf("        \"schedule_list\": [\n")
+    for index, schedule := range scheduleHolder {
     appConfigFile += fmt.Sprintf("            {%s}", schedule)
     if index < len(scheduleHolder) - 1 {
         appConfigFile += fmt.Sprintf(",")
     }
     appConfigFile += fmt.Sprintf("\n")
-	}
-	appConfigFile += fmt.Sprintf("        ],\n")
-	
-	appConfigFile += fmt.Sprintf("        \"task_list\": [\n")
-	for index, task := range taskHolder {
-	appConfigFile += fmt.Sprintf("            {%s}", task)
-	if index < len(taskHolder) - 1 {
+    }
+    appConfigFile += fmt.Sprintf("        ],\n")
+    
+    appConfigFile += fmt.Sprintf("        \"task_list\": [\n")
+    for index, task := range taskHolder {
+    appConfigFile += fmt.Sprintf("            {%s}", task)
+    if index < len(taskHolder) - 1 {
         appConfigFile += fmt.Sprintf(",")
     }
     appConfigFile += fmt.Sprintf("\n")
-	}
-	appConfigFile += fmt.Sprintf("        ],\n")
+    }
+    appConfigFile += fmt.Sprintf("        ],\n")
 
     appConfigFile += fmt.Sprintf("        \"webserver_route\": [\n")
-	for index, handler := range handlerHolder {
-	appConfigFile += fmt.Sprintf("            {%s}", handler)
+    for index, handler := range handlerHolder {
+    appConfigFile += fmt.Sprintf("            {%s}", handler)
     if index < len(handlerHolder) - 1 {
         appConfigFile += fmt.Sprintf(",")
     }
     appConfigFile += fmt.Sprintf("\n")
-	}
-	appConfigFile += fmt.Sprintf("        ],\n")
-	appConfigFile += fmt.Sprintf("        \"webserver_route_group\": [\n")
-	for index, group := range groupHolder {
-	appConfigFile += fmt.Sprintf("            {%s}", group)
+    }
+    appConfigFile += fmt.Sprintf("        ],\n")
+    appConfigFile += fmt.Sprintf("        \"webserver_route_group\": [\n")
+    for index, group := range groupHolder {
+    appConfigFile += fmt.Sprintf("            {%s}", group)
     if index < len(groupHolder) - 1 {
         appConfigFile += fmt.Sprintf(",")
     }
     appConfigFile += fmt.Sprintf("\n")
-	}
-	appConfigFile += fmt.Sprintf("        ]\n")
-	appConfigFile += fmt.Sprintf("}\n")
-	
-	source += fmt.Sprintf("\n\nvar appConfigFile = `%s`\n", appConfigFile)
+    }
+    appConfigFile += fmt.Sprintf("        ]\n")
+    appConfigFile += fmt.Sprintf("}\n")
+    
+    source += fmt.Sprintf("\n\nvar appConfigFile = `%s`\n", appConfigFile)
 
-	source += fmt.Sprintf("func main() {\n")
-	source += fmt.Sprintf("    err := vasc.InitInstance(\n")
-	source += fmt.Sprintf("        &global.VascApplication{\n")
-	source += fmt.Sprintf("            FuncMap: VascFuncMap,\n")
-	source += fmt.Sprintf("            Configuration: configFile,\n")
-	source += fmt.Sprintf("            AppConfiguration: appConfigFile,\n")
-	source += fmt.Sprintf("    })\n\n")
-	
-	source += fmt.Sprintf("    if err!=nil {\n")
-	source += fmt.Sprintf("        panic(err)\n")
-	source += fmt.Sprintf("        return\n")
-	source += fmt.Sprintf("    }\n")
-	source += fmt.Sprintf("    defer vasc.Close()\n")
-	source += fmt.Sprintf("\n")
-	source += fmt.Sprintf("    err = vasc.StartService()\n")
-	source += fmt.Sprintf("    if err!=nil {\n")
-	source +=             "        vasc.ErrorLog(\"Starting service failed: %s\", err.Error())\n"
-	source += fmt.Sprintf("        return\n")
-	source += fmt.Sprintf("    } else {\n")
-	source += fmt.Sprintf("        vasc.Wait()\n")
-	source += fmt.Sprintf("    }\n")
-	source += fmt.Sprintf("}\n")
+    source += fmt.Sprintf("func main() {\n")
+    source += fmt.Sprintf("    err := vasc.InitInstance(\n")
+    source += fmt.Sprintf("        &global.VascApplication{\n")
+    source += fmt.Sprintf("            FuncMap: VascFuncMap,\n")
+    source += fmt.Sprintf("            Configuration: configFile,\n")
+    source += fmt.Sprintf("            AppConfiguration: appConfigFile,\n")
+    source += fmt.Sprintf("    })\n\n")
+    
+    source += fmt.Sprintf("    if err!=nil {\n")
+    source += fmt.Sprintf("        panic(err)\n")
+    source += fmt.Sprintf("        return\n")
+    source += fmt.Sprintf("    }\n")
+    source += fmt.Sprintf("    defer vasc.Close()\n")
+    source += fmt.Sprintf("\n")
+    source += fmt.Sprintf("    err = vasc.StartService()\n")
+    source += fmt.Sprintf("    if err!=nil {\n")
+    source +=             "        vasc.ErrorLog(\"Starting service failed: %s\", err.Error())\n"
+    source += fmt.Sprintf("        return\n")
+    source += fmt.Sprintf("    } else {\n")
+    source += fmt.Sprintf("        vasc.Wait()\n")
+    source += fmt.Sprintf("    }\n")
+    source += fmt.Sprintf("}\n")
 
-	err = ioutil.WriteFile(*output, []byte(source), 0666)
-	if err != nil {
-		fmt.Println("Cannot write output file:" + err.Error())
-	}
-	
-	fmt.Printf("%s generated.\n", *output)
+    err = ioutil.WriteFile(*output, []byte(source), 0666)
+    if err != nil {
+        fmt.Println("Cannot write output file:" + err.Error())
+    }
+    
+    fmt.Printf("%s generated.\n", *output)
 }
