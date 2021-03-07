db4716a28f3c7ab68cb478c1226078d19faf5cb5
marxn <wywinxp@163.com>
2021-03-07 22:14:32 +0800

    no use

--- target/albj/mega_config/0.8.1.1/mega_config_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-servicepath=$1
-serviceuser=$2
-projectname=$3
-environment=$4
-
-cmd="cd $servicepath/$projectname; $servicepath/$projectname/$projectname -n $projectname -e $environment -p $servicepath/$projectname/$projectname.pid 1>$servicepath/$projectname/console.log 2>&1 &"
-su - $serviceuser -c "$cmd"
-
